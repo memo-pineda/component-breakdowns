34bd38a3446e97bb5b8141013a9809cf7c896d85
Guillermo Pineda <pinedag395@gmail.com>
2024-06-20 09:10:08 -0700

    Fetch Images with unsplash api

--- src/components/ImageCarousel/Carousel.tsx
+++ src/components/ImageCarousel/Carousel.tsx
@@ -15,6 +15,27 @@ interface LazyImageProps {
   isLoaded: boolean;
 }
 
+interface Image {
+  id: string;
+  alt_description: string;
+  urls: {
+    small: string;
+    regular: string;
+    thumb: string;
+  }
+}
+
+/** Todos
+ * - [ ] component recycling
+ * - [ ] list virtualization
+ * - [ ] Move prev/next buttons closer to images
+ * - [x] Fetch images from api
+ * - [ ] Ellipse the numbered buttons
+ * - [ ] Pressed states for buttons
+ */
+
+const url = `https://api.unsplash.com/photos/?client_id=yo4ApkueaPGe67S-rM5AkGF--_NWFgVkF-wkeHRaW5A`
+
 
 const handleEnds = (curr: number, min: number, max: number) => {
   if (curr < min) {
@@ -27,11 +48,46 @@ const handleEnds = (curr: number, min: number, max: number) => {
 }
 
 const LazyImage: FC<LazyImageProps> = ({ src, alt, isLoaded }) => {
-  return <img  className="image" src={src} alt={alt} width="500" height="500" loading={isLoaded ? 'eager' : 'lazy'} hidden={!isLoaded}/>
+  return <img  className={`image`} src={src} alt={alt} loading={isLoaded ? 'eager' : 'lazy'} hidden={!isLoaded}/>
+}
+
+const useFetchImages = (url: string) => {
+  const [images, setImages] = useState<Array<Image>>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchImages(url: string) {
+      setIsLoading(true);
+      try {
+        const resp = await fetch(url);
+        const images = await resp.json();
+        console.log("images", images);
+        setIsLoading(false);
+        setImages([...images]);
+        return images;
+      } catch (e) {
+        setError(true);
+      }
+    }
+
+    fetchImages(url);
+
+    () => {
+      
+    }
+  }, [url])
+  
+  return {
+    isLoading,
+    hasError,
+    images
+  }
 }
 
 
-const Carousel: FC<CarouselProps> = ({ images }) => {
+const Carousel: FC<CarouselProps> = () => {
+  const { images, isLoading, hasError } = useFetchImages(url);
   const [currentImg, setCurrentImg] = useState(0);
   const [loadedImages, setLoadedImages] = useState(images.reduce<Array<boolean>>((acc, _, idx) => {
     acc[idx] = idx == 0 ? true : false;
@@ -50,14 +106,22 @@ const Carousel: FC<CarouselProps> = ({ images }) => {
 
   useEffect(() => {
       containerRef.current?.scrollTo({
-        left: currentImg * 500,
+        left: currentImg * 400,
         behavior: 'smooth'
       })
   }, [currentImg]);
 
+  if (hasError) {
+    return <div>Error Loading Carousel</div>
+  }
+
+  if (isLoading) {
+    return <div>Images loading</div>
+  }
+
   return <>
     <div className="image-container" ref={containerRef}>
-      {images.map((image, idx) => <LazyImage src={image.src} alt={image.altText} isLoaded={loadedImages[idx]} key={`image_${idx}`}/>)}
+      {images.map((image, idx) => <LazyImage src={image.urls.small} alt={image.alt_description} isLoaded={idx === 0 ? true : loadedImages[idx]} key={image.id}/>)}
     </div>
     <div>
       <button onClick={() => goToSpecificPhoto(currentImg - 1)} onMouseEnter={() => loadImage(currentImg - 1)}>{'<'}</button>
